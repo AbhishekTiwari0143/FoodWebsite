8406ec2942bea525f355db3c3b396f9ded6495a4
Abhishek Tiwari <tiwariji2352003@gmail.com>
2023-06-10 12:24:23 +0530

    Replaced files

--- script.js
+++ script.js
@@ -16,5 +16,47 @@ tl.to(".Loading", {
 .to(".loader", {
   y: "-100%",
   duration: 1,  
-})
+},
+"up"
+)
+.to(
+  ".svg", {
+    opacity: 1,
+    delay: -1.7,
+    ease: "Expo.easeInOut",
+  },
+  "donut"
+)
+.to(
+  "#dotted", {
+    opacity: 1,
+    delay: -1.8,
+    ease: "Expo.easeInOut",
+  },
+  "donut"  
+)
+.to(
+  "#donut", {
+    opacity: 1,
+    scale: 1,
+    delay: -1.4,
+    ease: "Expo.easeInOut",
+    rotaionY: "+=15",
+    rotaionX: "+=15",
+    rotaionZ: "+=15",
+    duration: 3,
+  },
+  "up"
+)
+.from(
+  "#letter-1, #letter-2, #letter-3, #letter-4, #letter-5, #letter-6",{
+    opacity: 1,
+    duration: 1, 
+    scale: 0,
+    top: "40%",
+    left: "47%",
+  },
+  "up"
+);
+
 
